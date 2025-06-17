7011932bc517574b6f0f8ae7e69cf0e755a50e41
rickyMattio <163524689+rickyMattio@users.noreply.github.com>
2025-06-17 18:13:24 +0200

    Create AlgebraicIdentity.cpp

---
+++ Assignement1/test/AlgebraicIdentity.cpp
@@ -0,0 +1,11 @@
+//AlgebraicIdentityTest
+
+int algebraicIdentity(int n0) {
+    int n1 = n0 + 0;
+    int n2 = n1 - 1;
+    int n3 = 0 + n2;
+
+    int n4 = n3 * 1;
+    int n5 = n4 / 1;
+    return n5;
+}
